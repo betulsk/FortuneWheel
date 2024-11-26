bcf91483ade6b0a36aa96f342b147b11c5d3cae9
Betul <betul.s.99@hotmail.com>
2024-11-26 12:08:37 +0300

    WheelItem added

---
+++ Assets/Scripts/WheelItem/WheelItem.cs
@@ -0,0 +1,14 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WheelItem : MonoBehaviour
+{
+    [SerializeField] private Image _itemIconImage;
+    [SerializeField] private TMP_Text _itemText;
+
+    public void SetItemVisual()
+    {
+
+    }
+}
