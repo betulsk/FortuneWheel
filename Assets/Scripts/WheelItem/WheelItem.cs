9d59805710d7fdf41a947e40cc70e47440e5f4c6
Betul <betul.s.99@hotmail.com>
2024-11-28 15:33:41 +0300

    WheelRewardData added on Wheel Item

--- Assets/Scripts/WheelItem/WheelItem.cs
+++ Assets/Scripts/WheelItem/WheelItem.cs
@@ -7,8 +7,11 @@ public class WheelItem : MonoBehaviour
     [SerializeField] private Image _itemIconImage;
     [SerializeField] private TMP_Text _itemText;
 
+    public WheelRewardData RewardData;
+
     public void SetItemVisual(WheelRewardData rewardData)
     {
+        RewardData = rewardData;
         _itemIconImage.sprite = rewardData.RewardSprite;
         _itemIconImage.SetNativeSize();
         _itemText.SetText(rewardData.RewardMultiplier.ToString());
